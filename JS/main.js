b5db09858cc8978d3dd36f09ebbcc1f5b2e6d16e
“Steven” <steven160894@gmail.com>
2019-05-20 10:45:35 +0200

    agregado de axios y obtener la informacion de las peliculas desde un servidor

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,61 @@
-import films from './films.js';
+const IMG_PATH = 'http://image.tmdb.org/t/p/original';
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '3d2d2b0c9fe9449158a984016d34aa05';
+const API_POPULAR_URL = 'movie/popular';
+const API_CATEGORIES = 'genre/movie/list'
+let films = [];
+let generos = [];
+let botonBuscar = document.querySelector('#botonBuscar');
+
 window.addEventListener('load', () => {
-        pagina(films)
+
+        axios.get(API_URL + API_POPULAR_URL + '?api_key=' + API_KEY).then((response) => {
+                films = response.data.results;
+
+
+                axios.get(API_URL + API_CATEGORIES + '?api_key=' + API_KEY).then((resp) => {
+                        generos = resp.data.genres;
+
+                        films = films.map((pelicula) => {
+                                let arrayGeneros = pelicula['genre_ids'].map((id) => {
+                                        return generos.find((genero) => genero.id === id);
+                                });
+                                pelicula.genres = arrayGeneros;
+                                pelicula['poster_path'] = IMG_PATH + pelicula['poster_path'];
+                                pelicula['backdrop_path'] = IMG_PATH + pelicula['backdrop_path'];
+                                
+                                pelicula.stars = Math.round(pelicula['vote_average'] / 2);
+                                return pelicula;
+                        })
+                        botonBuscar.addEventListener('click', buscar);
+                        pagina(films);
+
+
+
+
+                })
+
+
+
+        });
+
+
+
+
 });
-
-function pagina(mostrar) {
-
-        const IMG = "http://image.tmdb.org/t/p/w185/"
-
-        let botonBuscar = document.querySelector('#botonBuscar');
-        botonBuscar.addEventListener('click', buscar);
+function pagina(films) {
         let divFilms = document.querySelector('#films');
+
         divFilms.innerHTML = '';
-        mostrar.forEach((film) => {
+        films.forEach((film) => {
+
 
                 let { title, vote_average, poster_path, release_date, id } = film;
+
                 let divFilm = document.createElement('div');
                 let date = release_date.split('-');
                 divFilm.innerHTML = `
-                        <a href="/peliculas.html?peliculas=${id}" ><img src="${IMG}${poster_path}" ></a>
+                        <a href="/peliculas.html?peliculas=${id}" ><img src="${poster_path}" ></a>
                         <p id="titulo"> ${title}</p1>
                         <p>${date[0]}</p>
                         <p id= "average">${vote_average}</p> `;
@@ -30,19 +68,15 @@ function buscar() {
         console.log('buscar');
         let inputElement = document.querySelector('#inputBusqueda');
         console.log(inputElement.value);
-        let clave = String(inputElement.value);
-        let titulos = films.map((function (nombres) {
-                return nombres;
-        }));
-        let filtrados = titulos.filter((titulo) => titulo.title.toLowerCase().indexOf(clave.toLowerCase()) >= 0);
-        console.log(filtrados);
-        if (filtrados.length == 0) {
+        console.log(films);
+        let find = inputElement.value.toLowerCase();
+        let film = films.filter((film) => film.title.toLowerCase().includes(find));
+        if (film.length == 0) {
                 pagina(films)
         } else {
-                pagina(filtrados)
+                pagina(film)
         };
 }
-
 
 
 
--- JS/peliculas.js
+++ JS/peliculas.js
@@ -1,68 +1,92 @@
-import films from './films.js';
-window.addEventListener('load', load);
+const IMG_PATH = 'http://image.tmdb.org/t/p/original';
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '3d2d2b0c9fe9449158a984016d34aa05';
+const API_POPULAR_URL = 'movie/popular';
+const API_CATEGORIES = 'genre/movie/list'
+let films = [];
+let generos = [];
+let botonBuscar = document.querySelector('#botonBuscar');
 
-function load(){
-    
-    let url = new URL(window.location.href);
-    let idpelicula = url.searchParams.get('peliculas').valueOf();
-    
-    console.log(idpelicula);
-    console.log(url);
-    let foundFilm = films.find((film) => film.id == idpelicula);
-    console.log(foundFilm);
-    pagina(foundFilm);
-}
+window.addEventListener('load', () => {
+
+        axios.get(API_URL + API_POPULAR_URL + '?api_key=' + API_KEY).then((response) => {
+                films = response.data.results;
+
+
+                axios.get(API_URL + API_CATEGORIES + '?api_key=' + API_KEY).then((resp) => {
+                        generos = resp.data.genres;
+
+                        films = films.map((pelicula) => {
+                                let arrayGeneros = pelicula['genre_ids'].map((id) => {
+                                        return generos.find((genero) => genero.id === id);
+                                });
+                                pelicula.genres = arrayGeneros;
+                                pelicula['poster_path'] = IMG_PATH + pelicula['poster_path'];
+                                pelicula['backdrop_path'] = IMG_PATH + pelicula['backdrop_path'];
+
+                                pelicula.stars = Math.round(pelicula['vote_average'] / 2);
+
+                                return pelicula;
+                        })
+                        botonBuscar.addEventListener('click', buscar);
+                        let url = new URL(window.location.href);
+                        let idpelicula = url.searchParams.get('peliculas').valueOf();
+
+                        console.log(idpelicula);
+                        console.log(url);
+                        let foundFilm = films.find((film) => film.id == idpelicula);
+                        console.log(foundFilm);
+                        pagina(foundFilm);
+
+
+
+
+                })
+
+
+
+        });
+
+
+
+
+});
 function pagina(movie) {
 
-    const IMG = "http://image.tmdb.org/t/p/w185/"
+        let divFilms = document.querySelector('#films');
+        divFilms.innerHTML = '';
+        let { title, vote_average, poster_path, release_date, overview, backdrop_path } = movie;
+        let divFilm = document.createElement('div');
+        divFilm.innerHTML = `
+        <img src="${poster_path}" ></a>
+        <p id="titulo"> ${title}</p1>
+        <p>${release_date}</p>
+        <p id= "average">${vote_average}</p> 
+        `;
 
-    let botonBuscar = document.querySelector('#botonBuscar');
-    botonBuscar.addEventListener('click', buscar);
-    let divFilms = document.querySelector('#films');
-    divFilms.innerHTML = '';
-    
+        divFilms.appendChild(divFilm);
+        let text = document.createElement('div');
+        text.innerHTML = `
+        <p1> ${overview} <p1>
+        `;
+        divFilms.appendChild(text);
+        document.querySelector('body').style.backgroundImage = `url(${backdrop_path})`;
+        document.querySelector('body').style.backgroundPosition = '0 0';
 
-            let { title, vote_average, poster_path, release_date, overview} = movie;
-            let divFilm = document.createElement('div');
-            let date = release_date.split('-');
-            divFilm.innerHTML = `
-                    <img src="${IMG}${poster_path}" >
-                    <p id="titulo"> ${title}</p1>
-                    <p>${date[0]}</p>
-                    <p id= "average">${vote_average}</p> 
-                    `;
-            divFilms.appendChild(divFilm);
-            let text = document.createElement('div');
-            text.innerHTML = `
-            <p1> ${overview} <p1>
-                     
-                    `;
-            divFilms.appendChild(text);
-    ;
 
 }
 function buscar() {
-    console.log('buscar');
-    let inputElement = document.querySelector('#inputBusqueda');
-    console.log(inputElement.value);
-    let clave = String(inputElement.value);
-    let titulos = films.map((function (nombres) {
-            return nombres;
-    }));
-    let filtrados = titulos.filter((titulo) => titulo.title.toLowerCase().indexOf(clave.toLowerCase()) >= 0);
-    console.log(filtrados);
-    if (filtrados.length == 0) {
-            pagina(films)
-    } else {
-            pagina(filtrados)
-    };
+        console.log('buscar');
+        let inputElement = document.querySelector('#inputBusqueda');
+        console.log(inputElement.value);
+        let find = inputElement.value.toLowerCase();
+        let film = films.filter((film) => film.title.toLowerCase().includes(find));
+        console.log(filtrados);
+        if (filtrados.length == 0) {
+                pagina(films)
+        } else {
+                pagina(film)
+        };
 }
 
 
-//<a href="/peliculas.html?peliculas=${peliculas}">${peliculas}</a>
-/*
-elementLi =.addEventListener('click', () =>{
-    window.location.assign('/ciudad.htl?ciudad='+ ciudad)
-    
-})
-*/
